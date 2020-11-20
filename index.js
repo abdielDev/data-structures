b54412f67b718f6ae6291a7809bae6a208127276
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-11-19 22:04:28 -0600

    The class deque and queue was built

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import decimalToBinary from "./algorithms/decimalToBinary.js";
 import baseConverter from "./algorithms/baseConverter.js";
 import { hanoi, hanoiStack } from "./algorithms/hanoi.js";
+import Queue from "./queues/queue.js";
 
 // console.log(decimalToBinary(233));	//	11101001
 // console.log(decimalToBinary(10));	//	1010
@@ -11,4 +12,17 @@ import { hanoi, hanoiStack } from "./algorithms/hanoi.js";
 // console.log(baseConverter(100345,	16));	//	187F9
 // console.log(baseConverter(100345,	35));	//	2BW0
 
-hanoiStack(7);
+// hanoiStack(7);
+const queue = new Queue();
+console.log(queue.isEmpty());
+queue.enqueue('John');
+queue.enqueue('Jack');
+console.log(queue.toString());
+queue.enqueue('Camila');
+console.log(queue.toString());	//	John,Jack,Camila
+console.log(queue.size());	//	outputs	3
+console.log(queue.isEmpty());	//	outputs	false
+queue.dequeue();	//	remove	John
+queue.dequeue();	//	remove	Jack
+console.log(queue.toString());	//	Camila
+console.log(queue.peek());
---
+++ queues/deque.js
@@ -0,0 +1,71 @@
+export default class Deque {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+  size() {
+    return this.count - this.lowestCount;
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  clear() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for(let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+  addFront(element) {
+    if(this.isEmpty()) {
+      this.addBack();
+    }
+    else if (this.lowestCount > 0) {
+      this.lowestCount--;
+      this.items[this.lowestCount] = element;
+    } else {
+      for(let i = this.count; i > 0; i--) {
+        this.items[i] = this.items[i - 1];
+      }
+      this.count++;
+      this.lowestCount = 0;
+      this.items = element;
+    }
+  }
+  addBack(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  removeFront() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    let removedElement = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    return removedElement;
+  }
+  removeBack() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+  peekFront() {
+    return this.isEmpty() ? undefined : this.items[this.lowestCount];
+  }
+  peekBack() {
+    return this.isEmpty() ? undefined : this.items[this.count - 1];
+  }
+}
