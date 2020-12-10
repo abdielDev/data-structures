37820a40898c950bffc54f2603bfbf384a800c4d
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-12-10 13:42:56 -0600

    The reverse string function was developed

---
+++ recursion/reverse.js
@@ -0,0 +1,10 @@
+function reverse(string){
+  // add whatever parameters you deem necessary - good luck
+  let resultString = ''
+  function helper(position) {
+      if(position < 0) return
+      resultString = resultString + string[position]
+      helper(position - 1)
+  }
+  helper(string.length - 1)
+}
