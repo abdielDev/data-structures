4ea73de3fa7e11778f05669e80ecb2229896802b
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-12-10 13:46:23 -0600

    The isPalidrome was developed

---
+++ recursion/isPalindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(string){
+  // add whatever parameters you deem necessary - good luck!
+  let resultString = ''
+  function helper(position) {
+      if(position < 0) return
+      resultString = resultString + string[position]
+      helper(position - 1)
+  }
+  helper(string.length - 1)
+  return resultString === string
+}
