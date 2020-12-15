baf5f1f982229ecfecdc71c5f81dce94dcfa8a11
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-12-15 12:10:15 -0600

    The code was optimized

--- sorting/bubble-sort/bubble_unoptimized.js
+++ sorting/bubble-sort/bubble_unoptimized.js
@@ -1,13 +1,17 @@
 // UNOPTIMIZED VERSION OF BUBBLE SORT
 function bubbleSort(arr){
+  let noSwap
   for(var i = arr.length; i > 0; i--){
+    noSwap = true
     for(var j = 0; j < i - 1; j++){
       if(arr[j] > arr[j+1]){
         var temp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = temp;
+        noSwap = false
       }
     }
+    if(noSwap) break;
   }
   return arr;
 }
@@ -17,13 +21,16 @@ function bubbleSort(arr) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   };
-
+  let noSwap
   for (let i = arr.length; i > 0; i--) {
+    noSwap = true
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        noSwap = false
       }
     }
+    if(noSwap) break;
   }
   return arr;
 }
