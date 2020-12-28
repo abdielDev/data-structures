3c613da100bc1341b73cc53386b3a1f74a8cb310
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-12-27 21:23:49 -0600

    The function radix sort was written

--- sorting/radix-sort/radix.js
+++ sorting/radix-sort/radix.js
@@ -1,3 +1,16 @@
+function digitCount(num) {
+  if(num === 0) return 1
+  return Math.floor(Math.log10(Math.abs(num))) + 1
+}
+
+function mostDigits(arr) {
+  let maxDigits = 0
+  for(let i = 0; i < arr.length; i++) {
+      maxDigits = Math.max(maxDigits, digitCount(arr[i]))
+  }
+  return maxDigits
+}
+
 function getDigit(num, place) {
   // my solution
   // const string = num.toString()
@@ -5,4 +18,17 @@ function getDigit(num, place) {
   return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10
 }
 
-getDigit(1992, 3)
+function radixSort(arr) {
+  let maxDigitCount = mostDigits(arr);
+  for(let k = 0; k < maxDigitCount; k++) {
+      let digitBuckets = Array.from({length: 10}, () => [])
+      for(let i = 0; i < arr.length; i++) {
+          let digit = getDigit(arr[i], k)
+          digitBuckets[digit].push(arr[i])
+      }
+      arr = [].concat(...digitBuckets)
+  }
+  return arr
+}
+
+radixSort([5,76,4532,234,543])
