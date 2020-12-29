f9da9227b89f7bfc4b7e1bd2901324cdbcd28b10
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-12-29 09:30:10 -0600

    The unshift method was built

--- data-structures/singlyLinkedList.js
+++ data-structures/singlyLinkedList.js
@@ -52,4 +52,16 @@ class SinglyLinkedList {
     }
     return temp;
   }
+  unshift(val) {
+    const newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
