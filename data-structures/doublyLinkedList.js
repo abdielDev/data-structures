eabb7590d018ef573ad0db4acb3d3890d04957a6
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-01-01 14:32:02 -0600

    The method was implemented

--- data-structures/doublyLinkedList.js
+++ data-structures/doublyLinkedList.js
@@ -25,4 +25,18 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if(!this.head) return undefined;
+    const removedNode = this.tail;
+    if(this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      removedNode.prev = null;
+    }
+    this.length--;
+    return removedNode;
+  }
 }
