3cc42438badd29714f909280a8ccf44d7786c227
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-01-02 19:35:32 -0600

    The queue was implemented

---
+++ data-structures/queues.js
@@ -0,0 +1,37 @@
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(value) {
+    const node = new Node(value);
+    if(!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.size++;
+  }
+  dequeue() {
+    if(!this.first) return null;
+    const temp = this.first;
+    if(this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = temp.next;
+    }
+    this.size--;
+    return temp;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
